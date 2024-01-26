adeab62def7d6313c19cfb0635fc3b1f6cc101bb
MDz1985 <m_d@mail.uk>
2024-01-26 22:20:16 +0100

    feat: implement cli/env.js functionality

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import { env } from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const rssRegex = /RSS_.*/;
+  Object.entries(env)
+    .filter(([key]) => rssRegex.test(key))
+    .forEach(([k, v]) => console.log(`${ k }=${ v }`))
 };
 
 parseEnv();
