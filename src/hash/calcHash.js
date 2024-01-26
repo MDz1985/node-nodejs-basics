effefcaa0db90535b4139aa44706413c41f95b90
MDz1985 <m_d@mail.uk>
2024-01-26 22:54:39 +0100

    feat: implement calcHash.js functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "node:fs";
+import { resolve } from "node:path";
+import { createHash } from "node:crypto";
+import { stdout } from "node:process";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = createReadStream(resolve('src','hash', 'files','fileToCalculateHashFor.txt'));
+  let data = '';
+  readStream.on('data', chunk => data += chunk);
+  readStream.on('end', () => {
+    const result = createHash('sha256')
+      .update(data)
+      .digest('hex');
+    stdout.write(result)
+  });
 };
 
 await calculateHash();
