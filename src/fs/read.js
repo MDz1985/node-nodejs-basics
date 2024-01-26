762e2a53e4b73f01898aca3a14d9d14303fb3297
MDz1985 <m_d@mail.uk>
2024-01-26 22:18:35 +0100

    feat: implement read.js functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from "node:fs";
+import { resolve } from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const folderPath = resolve('src', 'fs', 'files');
+  const src = resolve(folderPath, 'fileToRead.txt');
+  const error = new Error('FS operation failed');
+  readFile(src, (err, data) => {
+    if (err) throw error;
+    console.log(data.toString());
+  })
 };
 
 await read();
