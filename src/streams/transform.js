0792c3e217179022aacb7e11bbfc0d863edc5c83
MDz1985 <m_d@mail.uk>
2024-01-27 10:55:50 +0100

    feat: implement src/transform.js functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { stdin, stdout } from "node:process";
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  stdin.on('data', async (data) => {
+    const reverse = new Transform({
+      transform(chunk, _, callback) {
+        callback(null, String(chunk).split('').reverse().join(''))
+      },
+    });
+    let result = '';
+    for await (const buf of reverse.end(data)) {
+      result += buf.slice(1);
+    }
+    stdout.write(`${result}\r\n`);
+  })
 };
 
 await transform();
